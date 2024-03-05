c484980e6c309ba048399509c6e990bbe9f98238
Juan A. Sandino B <juan.armando.sandino.ba@outlook.com>
2024-03-05 15:43:16 -0600

    style and feat;
    modify style main, list clients in table, add input for search client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClientService } from './services/client.service';
 import { Client } from './models/client.model';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    RouterOutlet,
+    HttpClientModule,
+  ],
+  providers: [ ClientService ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppComponent {
   clients?: Client[];
+  termSearchLastName?: string = "";
 
   constructor(private clientService: ClientService ) { }
 
@@ -29,6 +42,36 @@ export class AppComponent {
   }
 
   deleteClient(id: number): void {
-    // this.deleteClient(id).subscribe( () => this.listClients());
+    this.clientService.deleteClient(id).subscribe( ( response ) => this.listClients());
   }
+
+  editActionClient(id?: number | null | undefined ): void {
+
+  }
+
+  deleteActionClient(id?: number | null | undefined ): void {
+
+  }
+
+  searchClient(): void {
+    if ( this.termSearchLastName?.toString().trim()==="") {
+      this.listClients();
+      return;
+    }
+    let auxClient:any [] = [];
+    let foundClients = this.clients?.forEach( (item ) => {
+      let auxName = item?.lastName?.toString().toLocaleLowerCase().trim();
+      let auxTermSearch =  this.termSearchLastName?.toString().toLocaleLowerCase().trim();
+      console.log( auxName, ' ', auxTermSearch )
+      if( auxName === auxTermSearch ) {
+        auxClient.push(item);
+      }
+    });
+
+    if ( auxClient.length > 0 ) {
+      this.clients = auxClient;
+    }
+
+  }
+
 }
